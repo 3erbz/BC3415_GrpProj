a9461b76da3a628fffb0a8df3a571d91289b101a
3erbz <107092081+3erbz@users.noreply.github.com>
2024-10-20 02:10:51 +0800

    updated files

--- website/pages/explore.py
+++ website/pages/explore.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
+import textblob
 
 explore_page= Blueprint ('explore_page', __name__, template_folder='/templates')
 
@@ -9,7 +10,13 @@ def explore ():
     return render_template ("explore.html", user=current_user)
 
 
-@explore_page.route ('/explore/result', methods = ['GET'])
-@login_required
-def result ():
-    return render_template ("explore_result.html", user=current_user)
+@explore_page.route ('/process', methods = ['GET','POST'])
+def process_data ():   
+    data  = request.form.get('data')
+    res = data.upper()
+    return res
+
+# @explore_page.route ('/explore/result', methods = ['GET','POST'])
+# def process_data2 ():   
+#     data  = request.form
+#     return render_template ("explore_result.html", user=current_user)
