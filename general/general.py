8bf8a352777ed10de1ca3c5da214e68e78eb4747
3erbz <107092081+3erbz@users.noreply.github.com>
2024-10-19 17:28:23 +0800

    launch new project

---
+++ general/general.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, render_template
+
+general_bp = Blueprint('general', __name__,
+                       template_folder='/templates')
+
+@general_bp.route('/about')
+def about():
+    return render_template('about.html')
