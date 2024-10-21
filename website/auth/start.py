1edc07d09321569e47ccdc799dc59ae3c514b00f
3erbz <107092081+3erbz@users.noreply.github.com>
2024-10-21 22:24:18 +0800

    updated files

--- website/auth/start.py
+++ website/auth/start.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, login_required, logout_user, current_user
+import requests
 
 auth_views = Blueprint ('auth_views', __name__, template_folder='website/templates')
 
@@ -64,7 +65,36 @@ def sign_up ():
             db.session.add (new_user)
             db.session.commit ()
             flash ('Account created', category='success')
-            return redirect (url_for('views.home'))
+            return redirect (url_for('auth_views.home'))
 
     return render_template ("sign_up.html", user=current_user)
 
+@auth_views.route ('/', methods = ['GET'])
+@login_required
+def home ():
+    api_key = "cc3747f4309f44bb9cc0c5d2bbd336df"
+    url = f"https://newsapi.org/v2/everything?q=scam&apiKey={api_key}"
+    response = requests.get (url)
+
+    count = 0
+    article_title = []
+    article_description = []
+    article_url = []
+    img_url = []
+
+    data = response.json ()
+    for key, value in data.items ():
+        if key == "articles":
+            for article in value:
+                if article ["title"] != "[Removed]" and article["url"] != "None" and article["urlToImage"] is not None and len (article["description"]) > 100:
+                    article_title.append (article ["title"])
+                    article_description.append (article ["description"])
+                    article_url.append (article ["url"])
+                    img_url.append (article ["urlToImage"])
+                    
+                    count += 1 
+
+                    if count == 6:
+                        break
+
+    return render_template ("home.html", user=current_user, title=article_title, description=article_description, url=article_url, img=img_url)
