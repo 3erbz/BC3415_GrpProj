054522dbf2dd111e710faf17a1cb584641fb21a4
3erbz <107092081+3erbz@users.noreply.github.com>
2024-10-22 17:32:15 +0800

    created rende version of app

---
+++ render/website/pages/explore.py
@@ -0,0 +1,46 @@
+from flask import Blueprint, render_template, request
+from flask_login import login_required, current_user
+import speech_recognition as sr
+import textblob
+
+explore_page= Blueprint ('explore_page', __name__, template_folder='/templates')
+
+@explore_page.route ('/explore', methods = ['GET'])
+@login_required
+def explore ():
+    return render_template ("explore.html", user=current_user)
+
+@explore_page.route ('/explore/result', methods=['GET', 'POST'])
+@login_required
+def explore_result ():
+    data_received = {}       
+    if request.method == 'POST':
+        # receiving data
+        text_data = request.form.get('text_data')
+        speech_data = request.form.get ('speech_data')
+        audio_data = request.files ['audio_data']
+
+        #convert audio data to text
+        recognizer = sr.Recognizer()
+        audioFile = sr.AudioFile (audio_data)
+        with audioFile as source:
+            data = recognizer.record(source)
+        audio_data = recognizer.recognize_google (data, key=None)
+        # maybe can use google genai, would require api key to be kept in environment
+
+        # append data to dictionary
+        data_received ["text"] = text_data
+        data_received ["speech"] = speech_data
+        data_received ["audio"] = audio_data
+    
+    # analysing data
+    analysis_data = {}
+    for data, content in data_received.items ():
+        if content == None or content != "":
+            analysis = textblob.TextBlob (content).sentiment
+            analysis_data [data] = {"polarity": analysis.polarity, "subjectivity": analysis.subjectivity}
+
+        
+    return render_template ("explore_result.html",user=current_user,
+                            data_received=data_received, 
+                            analysis_data=analysis_data)
