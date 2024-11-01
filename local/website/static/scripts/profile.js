a9e0dd8b56eea20b594b494dd2c90064368edff0
3erbz <107092081+3erbz@users.noreply.github.com>
2024-11-01 16:36:07 +0800

    updated all files

--- local/website/static/scripts/profile.js
+++ local/website/static/scripts/profile.js
@@ -2,12 +2,14 @@ function generateImage() {
   const firstName = document.getElementById("firstName").innerHTML;
   console.log(firstName + " was sent");
   $.ajax({
-    url: "/process",
+    url: "/generate-image",
     type: "POST",
     contentType: "application/json",
     data: JSON.stringify({ firstName: firstName }),
     success: function (response) {
       console.log(response);
+      const img = document.getElementById("img");
+      img.src = `data:image/jpeg;base64,${response["img"]}`;
     },
     error: function (error) {
       console.log(error);
